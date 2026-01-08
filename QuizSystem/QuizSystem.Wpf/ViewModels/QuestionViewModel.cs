46a4c072ed244aa516723c1e89ba9853c8ade53c
tokolorado <tobiasrottmann@interia.pl>
2026-01-08 11:08:24 +0100

    Add QuestionViewModel
    Add QuestionViewModel

---
+++ QuizSystem/QuizSystem.Wpf/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,34 @@
+﻿using QuizSystem.Core.Domain.Interfaces;
+
+namespace QuizSystem.Wpf.ViewModels
+{
+    /// <summary>
+    /// Pytanie w UI: treść + lista opcji.
+    /// </summary>
+    public class QuestionViewModel
+    {
+        public Guid Id { get; }
+
+        public string Content { get; }
+
+        public List<AnswerOptionViewModel> Options { get; }
+
+        public QuestionViewModel(IQuestion question)
+        {
+            Id = question.Id;
+            Content = question.Content;
+
+            Options = question.Answers
+                .Select(a => new AnswerOptionViewModel(a.Id, a.Text))
+                .ToList();
+        }
+
+        public IEnumerable<Guid> GetChosenAnswerIds()
+        {
+            return Options
+                .Where(o => o.IsSelected)
+                .Select(o => o.Id)
+                .ToList();
+        }
+    }
+}
--- QuizSystem/QuizSystem.Wpf/obj/Debug/net10.0-windows/QuizSystem.Wpf.AssemblyInfo.cs
+++ QuizSystem/QuizSystem.Wpf/obj/Debug/net10.0-windows/QuizSystem.Wpf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizSystem.Wpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b380d52c8ea1fb2eaf3c2a7245c4f53c23c05027")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b1dabe11f196807b6f1ceae8288b09c2014899")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizSystem.Wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizSystem.Wpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
