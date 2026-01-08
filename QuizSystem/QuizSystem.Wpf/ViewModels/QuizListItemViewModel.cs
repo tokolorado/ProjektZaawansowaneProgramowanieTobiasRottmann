2e1fc5fc3b43ee9a060ab8c32d43457cb005a746
tokolorado <tobiasrottmann@interia.pl>
2026-01-08 13:17:11 +0100

    Add QuizListItemViewModel
    Add QuizListItemViewModel

---
+++ QuizSystem/QuizSystem.Wpf/ViewModels/QuizListItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using QuizSystem.Core.Domain.Interfaces;
+
+namespace QuizSystem.Wpf.ViewModels
+{
+    /// <summary>
+    /// Element listy quizów do menu wyboru.
+    /// Trzyma referencję do quizu (IQuiz) + dane do wyświetlenia.
+    /// </summary>
+    public class QuizListItemViewModel
+    {
+        public IQuiz Quiz { get; }
+
+        public Guid Id => Quiz.Id;
+        public string Title => Quiz.Title;
+        public string? Description => Quiz.Description;
+        public int QuestionCount => Quiz.Questions.Count;
+
+        public QuizListItemViewModel(IQuiz quiz)
+        {
+            Quiz = quiz;
+        }
+    }
+}
--- QuizSystem/QuizSystem.Wpf/obj/Debug/net10.0-windows/QuizSystem.Wpf.AssemblyInfo.cs
+++ QuizSystem/QuizSystem.Wpf/obj/Debug/net10.0-windows/QuizSystem.Wpf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizSystem.Wpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dccd5e1735fa31229a8a9580546a1722071b2698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624ff861623e0a4025b593cb59bc640d784a7c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizSystem.Wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizSystem.Wpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
